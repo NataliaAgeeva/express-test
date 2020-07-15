307a6e3b9ceaed5f0bde50c0a262ab06b3b5bf8b
Natalia Ageeva <natalia.ageeva.a@yandex.ru>
2020-07-15 14:48:51 +0300

    cards modified

--- routes/cards.js
+++ routes/cards.js
@@ -2,8 +2,9 @@ const getCards = require('express').Router();
 const fs = require('fs').promises;
 const path = require('path');
 
+const filePath = path.resolve('data', 'cards.json');
+
 getCards.get('/', (req, res) => {
-  const filePath = path.resolve('data', 'cards.json');
   fs.readFile(filePath)
     .then((data) => {
       res.status(200).send(JSON.parse(data));
