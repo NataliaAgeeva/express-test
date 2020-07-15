c1d1d38fff9b611939f0a123297c627c3d16d58f
Natalia Ageeva <natalia.ageeva.a@yandex.ru>
2020-07-15 14:47:58 +0300

    users modified

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,32 @@
 const getUsers = require('express').Router();
-const users = require('../data/users');
+const fs = require('fs').promises;
+const path = require('path');
+
+const filePath = path.resolve('data', 'users.json');
 
 getUsers.get('/:id', (req, res) => {
-  const user = users.find(({ _id: id }) => id === req.params.id);
-  if (!user) {
-    res.status(404).json({ message: 'Нет пользователя с таким id' });
-    return;
-  }
-  res.status(200).json(user);
+  fs.readFile(filePath)
+    .then((data) => {
+      const userArray = JSON.parse(data);
+      const user = userArray.find(({ _id: id }) => id === req.params.id);
+      if (!user) {
+        res.status(404).json({ message: 'Нет пользователя с таким id' });
+      }
+      res.status(200).send(user);
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Запрашиваемый файл не найден' });
+    });
 });
 
 getUsers.get('/', (req, res) => {
-  res.json(users);
+  fs.readFile(filePath)
+    .then((data) => {
+      res.status(200).send(JSON.parse(data));
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Запрашиваемый файл не найден' });
+    });
 });
 
 module.exports = getUsers;
