e643191d43d1f8defaabe547f94faa9943a3c4c8
Natalia Ageeva <natalia.ageeva.a@yandex.ru>
2020-07-15 13:38:31 +0300

    router for cards modified

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,16 @@
 const getCards = require('express').Router();
-const cards = require('../data/cards');
+const fs = require('fs').promises;
+const path = require('path');
 
 getCards.get('/', (req, res) => {
-  res.status(200).json(cards);
+  const filePath = path.resolve('data', 'cards.json');
+  fs.readFile(filePath)
+    .then((data) => {
+      res.status(200).send(JSON.parse(data));
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Запрашиваемый файл не найден' });
+    });
 });
 
 module.exports = getCards;
